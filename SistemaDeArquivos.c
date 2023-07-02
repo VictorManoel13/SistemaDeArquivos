5cb1c9b9523dcc6ecc66257cfb62ba109f05624d
AKOB <112267542+AllanKaio30@users.noreply.github.com>
2023-07-02 12:39:35 -0300

    Update SistemaDeArquivos.c

--- SistemaDeArquivos.c
+++ SistemaDeArquivos.c
@@ -1,4 +1,33 @@
-#include <stdio.h>
-main(){
-	printf("Ola\n");
+#include<stdio.h>
+
+struct Arquivo{
+
+   char Nome[30];
+   int Tamanho;
+   int bloco_inicio;
+   int bloco_fim;
+
+};
+
+// sao 2048 bits divididos em blocos de 8
+int Disco[256];
+
+void criar_arquivo(){
+};
+
+void remover_arquivo(){
+};
+
+void desfragmentar(){
+};
+
+void exibir_info(){
+};
+
+
+
+int main(){
+
+   
+
 }
