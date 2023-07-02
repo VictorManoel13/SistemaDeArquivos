7b685a834560e714e4dee5e722903b8cb01f67ee
AKOB <112267542+AllanKaio30@users.noreply.github.com>
2023-07-02 14:11:53 -0300

    Update SistemaDeArquivos.c

--- SistemaDeArquivos.c
+++ SistemaDeArquivos.c
@@ -1,33 +1,66 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-struct Arquivo{
+struct Dados_arquivo{
 
    char Nome[30];
-   int Tamanho;
-   int bloco_inicio;
-   int bloco_fim;
+   int Bits;
+   int Blocos;
+   int Bloco_inicio;
 
 };
 
 // sao 2048 bits divididos em blocos de 8
-int Disco[256];
+struct Dados_arquivo Disco[256];
+struct Dados_arquivo Arquivo;
 
 void criar_arquivo(){
-};
+   
+   printf("Como deseja nomear o arquivo?");
+   scanf("%[^\n]", &Arquivo.Nome);
+   printf("Quantos bits o arquivo possui?\n");
+   scanf("%d", &Arquivo.bits);
+
+   Arquivo.Blocos = (Arquivo.Bits/8);
+
+
+   
+
+}
+
+// verifica se ja existe um arquivo com o nome escolhido
+void verificar_existencia(){
+   char op;
+   for(int i = 0; i <= 256; i++){
+   {}
+      if(strcmp(Arquivo.Nome, Disco[i].Nome) == 0){
+         printf("Um arquivo com esse nome ja existe!\n");
+         printf("Gostaria de tentar outro nome? s/n");
+         scanf("%c", &op);
+         if(op == 's' || op == 'S'){
+            criar_arquivo();
+         } else{
+            printf("Obrigado por usar nosso programa!");
+            exit(1);
+         }
+      }
+   }
+}
 
 void remover_arquivo(){
-};
+}
 
 void desfragmentar(){
-};
+}
 
 void exibir_info(){
-};
+}
 
 
 
 int main(){
 
-   
+   criar_arquivo();
 
 }
